06f5770129e49dba3c2faae79d7012b1482c8c85
dufrane <dmitriy.vasilenko@gmail.com>
2022-08-26 22:29:31 +0400

    add swipe to delete

--- Todoey/Controller/CategoryViewController.swift
+++ Todoey/Controller/CategoryViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import SwipeCellKit
 
 class CategoryViewController: UITableViewController {
     
@@ -16,6 +17,8 @@ class CategoryViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.rowHeight = 80
 
         loadCategories()
     }
@@ -53,45 +56,85 @@ class CategoryViewController: UITableViewController {
         } catch {
             print("Error saving category \(error)")
         }
+        
         tableView.reloadData()
     }
 
     func loadCategories() {
         
         categories = realm.objects(Category.self)
+        
         tableView.reloadData()
+        
     }
 }
 
-// MARK: - Table view data source
+// MARK: - TableView DataSource
 
 extension CategoryViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return categories?.count ?? 1
     }
-
+ 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! SwipeTableViewCell
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No categories added yet"
         
+        cell.delegate = self
         return cell
     }
 }
 
-// MARK: - Table view delegate
+// MARK: - TableView Delegate
 
 extension CategoryViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
         performSegue(withIdentifier: "goToItems", sender: self)
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
         let destinationVC = segue.destination as! TodoListViewController
         
         if let indexPath = tableView.indexPathForSelectedRow {
             destinationVC.seletedCategory = categories?[indexPath.row]
         }
+    }
+}
+
+
+// MARK: - SwipeTableViewCellDelegate
+
+
+extension CategoryViewController: SwipeTableViewCellDelegate {
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            if let category = self.categories?[indexPath.row] {
+                do {
+                    try self.realm.write{
+                        self.realm.delete(category)
+                    }
+                } catch {
+                    print("Error deleting category, \(error )")
+                }
+//                tableView.reloadData()
+            }
+        }
+        deleteAction.image = UIImage(named: "delete-icon")
+
+        return [deleteAction]
+    }
+    
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        return options
     }
 }
--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -22,7 +22,47 @@ class TodoListViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    // MARK: - UITableViewDataSouse
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoItems?.count ?? 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
+        if let item = self.todoItems?[indexPath.row] {
+            
+            cell.textLabel?.text = item.title
+            cell.accessoryType = item.done ? .checkmark : .none
+        } else {
+            
+            cell.textLabel?.text = "No items added"
+        }
+     
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let item = self.todoItems?[indexPath.row] {
+            do {
+                try realm.write {
+                    item.done = !item.done
+                }
+            } catch {
+                print("Error saving done status, \(error )")
+            }
+        }
         tableView.reloadData()
+        
+         tableView.deselectRow(at: indexPath, animated: true)
     }
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -38,6 +78,7 @@ class TodoListViewController: UITableViewController {
                     try self.realm.write{
                         let newItem = Item()
                         newItem.title = textFileld.text!
+                        newItem.dateCreated = Date()
                         currentCategory.items.append(newItem)
                     }
                 } catch {
@@ -65,71 +106,25 @@ class TodoListViewController: UITableViewController {
     }
 }
 
-
-// MARK: - UITableViewDataSouse
-
-extension TodoListViewController {
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return todoItems?.count ?? 1
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        if let item = todoItems?[indexPath.row] {
-            cell.textLabel?.text = item.title
-            cell.accessoryType = item.done ? .checkmark : .none
-        } else {
-            cell.textLabel?.text = "No items added"
-        }
-        return cell
-    }
-    
-}
-
-// MARK: - UITableViewDelegate
-
-extension TodoListViewController {
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        if let item = todoItems?[indexPath.row] {
-            do {
-                try realm.write {
-                    item.done = !item.done
-                }
-            } catch {
-                print("Error saving done status, \(error )")
-            }
-        }
-        
-
-        tableView.reloadData()
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-}
-
 // MARK: - UISearchBarDelegate
 
 extension TodoListViewController: UISearchBarDelegate {
-    //
-    //    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-    //        let request: NSFetchRequest<Item> = Item.fetchRequest()
-    //
-    //        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
-    //
-    //        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
-    //        loadItems(with: request, predicate: predicate)
-    //    }
-    //    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-    //        if searchBar.text?.count == 0 {
-    //            loadItems()
-    //
-    //            DispatchQueue.main.async {
-    //                searchBar.resignFirstResponder()
-    //            }
-    //        }
-    //    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        todoItems = todoItems?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "dateCreated", ascending: true)
+        
+        tableView.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
 }
 
